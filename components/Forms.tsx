62ab8440c4d91a31e04c9d20fed731ea49947915
Shoohei <rouillardtonyp@gmail.com>
2020-12-27 18:32:26 +0900

    Example 1 form

--- components/Forms.tsx
+++ components/Forms.tsx
@@ -2,8 +2,26 @@ import React from "react";
 import LifeCycleComponent from "../utilities/LifeCycleComponent";
 
 class Forms extends LifeCycleComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: "Initial data..."
+    };
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState(e) {
+    this.setState({ data: e.target.value });
+  }
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <input type = "text" value = {this.state.data} onChange = {this.updateState} />
+        <p>{this.state.data}</p>
+      </div>
+    );
   }
 }
 

81f38093215337e99b7cd0b6f6363c27fe80430f
Shoohei <rouillardtonyp@gmail.com>
2021-01-31 18:00:20 +0900

    Redux examples finished

--- components/redux/actions/actions.js
+++ components/redux/actions/actions.js
@@ -3,6 +3,7 @@ export const ADD_TODO = "ADD_TODO";
 let nextTodoId = 0;
 
 export function addTodo(text) {
+  console.log("addTodo: " + nextTodoId);
   return {
     type: ADD_TODO,
     id: nextTodoId++,
--- components/redux/components/Todo.js
+++ components/redux/components/Todo.js
@@ -3,6 +3,10 @@ import LifeCycleComponent from "../../../utilities/LifeCycleComponent";
 
 export default class Todo extends LifeCycleComponent {
   render() {
-    return <li>{this.props.text}</li>;
+    return (
+      <li>
+        {this.props.text} ({this.props.id})
+      </li>
+    );
   }
 }

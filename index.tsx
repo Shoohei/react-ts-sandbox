f479cccb4ce2c66dc382aa069b2584c6f92932a6
Shoohei <rouillardtonyp@gmail.com>
2021-01-31 16:23:00 +0900

    NotFound finished

--- index.tsx
+++ index.tsx
@@ -1,36 +1,57 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { Route, NavLink, BrowserRouter as Router } from "react-router-dom";
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
 import WithInputParameters from "./components/WithInputParameters";
 import Forms from "./components/Forms";
 import ConditionalRendering from "./components/ConditionalRendering";
 import Lists from "./components/Lists";
 import "./style.css";
 
+const NotFound = () => <h1>Not Found </h1>;
+
 const routing = (
   <Router>
     <ul className="examples">
+      <li>
+        <NavLink to="/" exact activeStyle={{ color: "red" }}>
+          Main
+        </NavLink>
+      </li>
       <li>
         <NavLink to="/wip" exact activeStyle={{ color: "red" }}>
           WithInputParameters
         </NavLink>
       </li>
       <li>
-        <NavLink to="/forms" exact activeStyle={{ color: "red" }}>Forms</NavLink>
+        <NavLink to="/forms" exact activeStyle={{ color: "red" }}>
+          Forms
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/cr" exact activeStyle={{ color: "red" }}>ConditionalRendering</NavLink>
+        <NavLink to="/cr" exact activeStyle={{ color: "red" }}>
+          ConditionalRendering
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/lists" exact activeStyle={{ color: "red" }}>Lists</NavLink>
+        <NavLink to="/lists" exact activeStyle={{ color: "red" }}>
+          Lists
+        </NavLink>
       </li>
     </ul>
     <hr />
-    <Route exact path="/" component={Lists} />
-    <Route path="/wip" component={WithInputParameters} />
-    <Route path="/forms" component={Forms} />
-    <Route path="/cr" component={ConditionalRendering} />
-    <Route path="/lists" component={Lists} />
+    <Switch>
+      <Route exact path="/" component={Lists} />
+      <Route path="/wip" component={WithInputParameters} />
+      <Route path="/forms" component={Forms} />
+      <Route path="/cr" component={ConditionalRendering} />
+      <Route path="/lists" component={Lists} />
+      <Route component={NotFound} />
+    </Switch>
   </Router>
 );
 

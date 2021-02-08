47f63e3ac3fe302e8729b1d44999c230e2498190
Shoohei <rouillardtonyp@gmail.com>
2021-02-08 16:29:03 +0900

    Test

---
+++ components/redux2/Redux2.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import LifeCycleComponent from "../../utilities/LifeCycleComponent";
+
+class Redux2 extends LifeCycleComponent {
+  render() {
+    return <p>Tony</p>;
+  }
+}
+export default Redux2;
--- components/redux2/containers/AddTodo.js
+++ components/redux2/containers/AddTodo.js
@@ -1,27 +1,32 @@
 import React from "react";
+import LifeCycleComponent from "../../../utilities/LifeCycleComponent";
+
 import { connect } from "react-redux";
 import { addTodo } from "../actions";
 
-const AddTodo = ({ dispatch }) => {
-  let input;
-
-  return (
-    <div>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          if (!input.value.trim()) {
-            return;
-          }
-          dispatch(addTodo(input.value));
-          input.value = "";
-        }}
-      >
-        <input ref={node => (input = node)} />
-        <button type="submit">Add AddTodo</button>
-      </form>
-    </div>
-  );
-};
+class AddTodo extends LifeCycleComponent {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Provider>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            if (!input.value.trim()) {
+              return;
+            }
+            dispatch(addTodo(input.value));
+            input.value = "";
+          }}
+        >
+          <input ref={node => (input = node)} />
+          <button type="submit">Add AddTodo</button>
+        </form>
+      </Provider>
+    );
+  }
+}
 
 export default connect()(AddTodo);
